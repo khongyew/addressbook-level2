f8ee09f91cb133d33912e64bd6f8fb7b3ee304aa
Lim Khong Yew <e0003227@u.nus.edu>
2016-09-07 22:54:13 +0800

    Add attributes tag, person and status

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,8 +1,22 @@
 package seedu.addressbook.data.tag;
 
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.tag.Tag;
+
 public class Tagging {
 	
-	public Tagging(){
-		
+	private final Tag tag;
+	private final Person person;
+	
+	private enum Status{
+		ADDED, DELETED
+	}
+	
+	private Status status;
+	
+	public Tagging(Tag tag, Person person, Status status){
+		this.tag = tag;
+		this.person = person;
+		this.status = status;
 	}
 }
